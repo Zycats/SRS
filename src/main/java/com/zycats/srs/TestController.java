e2285c0f62f61fe20b9850f095d2e9cf0741ebfb
Amar Pawar <pawaramar58@gmail.com>
2018-09-07 18:26:59 +0530

    user home UI done.

--- src/main/java/com/zycats/srs/TestController.java
+++ src/main/java/com/zycats/srs/TestController.java
@@ -3,13 +3,15 @@ package com.zycats.srs;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class TestController {
 
-	@RequestMapping
+	/*@RequestMapping
 	public String test(Authentication auth, HttpServletRequest request) {
 		// System.out.println("amhi print kela : " + request.getRemoteAddr());
 		return String.format(
@@ -19,6 +21,19 @@ public class TestController {
 				auth.getCredentials(),
 				auth.isAuthenticated());
 
+	}*/
+	
+	@GetMapping("/home")
+	public String home()
+	{
+		return "home-view";
 	}
+	
+	@GetMapping("/autha")
+	public String getAuth()
+	{
+		return "auth";
+	}
+
 
 }
