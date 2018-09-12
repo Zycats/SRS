371d4a0a12c40f6e3edb8b272b29bdf8c3fd2a8d
aditya.kurhade <adikrhd@gmail.com>
2018-09-12 18:10:58 +0530

    controllers merged

--- src/main/java/com/zycats/srs/controller/EmployeeRestController.java
+++ src/main/java/com/zycats/srs/controller/EmployeeRestController.java
@@ -1,7 +1,5 @@
 package com.zycats.srs.controller;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zycats.srs.dto.EmployeeLocationDTO;
 import com.zycats.srs.entity.Employee;
-import com.zycats.srs.entity.Status;
-import com.zycats.srs.entity.Ticket;
 import com.zycats.srs.service.IEmployeeService;
 import com.zycats.srs.service.ITicketService;
 
@@ -59,38 +55,7 @@ public class EmployeeRestController {
 	}
 	
 	
-	// ticket related rest controls
 	
-	//accepts status and employeeId and returns all the tickets
-		@RequestMapping(value = "get/status/empId", method= RequestMethod.POST,
-				consumes = "application/json",
-				produces="application/json")
-		public Iterable<Ticket> getAllTicketByStatus(@RequestBody Map<String, String> data){
-			
-			return ticketService.getTicketsByStatusAndEngineer(Status.valueOf(data.get("status")),
-					data.get("employeeId"));
-		}
-	
-	//accepts category and employeeId and returns all the tickets
-			@RequestMapping(value = "get/category/empId", method= RequestMethod.POST,
-					consumes = "application/json",
-					produces="application/json")
-			public Iterable<Ticket> getAllTicketByCategory(@RequestBody Map<String, String> data){
-				
-				return ticketService.findAllTicketsByCategory(Integer.parseInt(data.get("category_id")),
-						data.get("employeeId"));
-			}
-				
-	//accepts sub_category and employeeId and returns all the tickets
-			@RequestMapping(value = "get/sub_category/empId", method= RequestMethod.POST,
-					consumes = "application/json",
-					produces="application/json")
-			public Iterable<Ticket> getAllTicketBySubCategory(@RequestBody Map<String, String> data){
-				
-				return ticketService.findAllTicketsBySubCategory(Integer.parseInt(data.get("sub_category_id")),
-						data.get("employeeId"));
-			}		
-		
 	
 	
 	
