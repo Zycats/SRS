44282df05a12d82cfc9db8f336058311f9746587
Suyog <itssuyog96@gmail.com>
2018-09-28 03:03:20 +0530

    huge jpa query bug

--- src/main/java/com/zycats/srs/repository/EmployeeRepository.java
+++ src/main/java/com/zycats/srs/repository/EmployeeRepository.java
@@ -10,6 +10,6 @@ import com.zycats.srs.entity.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, String> {
 
-	@Query("SELECT  e FROM Employee e WHERE e.role = com.zycats.srs.entity.Role.MANAGER AND e.id LIKE %search%")
+	@Query("SELECT  e FROM Employee e WHERE e.role = com.zycats.srs.entity.Role.MANAGER AND e.id LIKE %:search%")
 	public List<Employee> getManagersByName(@Param("search") String search);
 }
