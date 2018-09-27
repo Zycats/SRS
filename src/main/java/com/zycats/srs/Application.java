b2ea0c18ffbc9b1a58e66cf41abf33bd17ec1b50
megha.shanbhag <megha.shanbhag@ZIPLSY1638.zycus.net>
2018-09-27 18:55:35 +0530

    staging commit

--- src/main/java/com/zycats/srs/Application.java
+++ src/main/java/com/zycats/srs/Application.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.zycats.srs.repository")
-@EnableScheduling
+//@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
