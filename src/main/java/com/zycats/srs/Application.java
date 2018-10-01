8f6b7931f1ef942bdb25568478bd7e4cf0a25047
Suyog Gadhave <itssuyog96@gmail.com>
2018-10-01 12:02:27 +0530

    scheduler off

--- src/main/java/com/zycats/srs/Application.java
+++ src/main/java/com/zycats/srs/Application.java
@@ -5,11 +5,10 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.zycats.srs.repository")
-@EnableScheduling
+// @EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
